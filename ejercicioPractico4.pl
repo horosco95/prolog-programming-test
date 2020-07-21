1fff1428400f5609716ffcaeee9b3542994ff8ea
Hermes Orosco <hermesorosco2010@gmail.com>
2020-07-21 20:04:40 -0300

    completando ejercicios clase 07-21

--- ejercicioPractico4.pl
+++ ejercicioPractico4.pl
@@ -123,3 +123,35 @@ afincado(Agente):- tarea(Agente, _, Ubicacion),
     forall(tarea(Agente, _, OtraUbicacion), esIgual(Ubicacion, OtraUbicacion)).
 
 esIgual(Ubicacion,Ubicacion).
+
+%Punto 4%
+cadenaDeMando([_]).
+cadenaDeMando([Jefe,Subor|Resto]):- jefe(Jefe, Subor), cadenaDeMando([Subor|Resto]).
+
+%cadenaDeMando([jefeSupremo, vega, vigilanteDelBarrio]).
+%true
+
+%cadenaDeMando([jefeSupremo, vega, sargentoGarcia]).
+%false
+
+%Punto 5
+agentePremiado(Agente):- totalPuntos(Agente, Puntos), 
+    forall(totalPuntos(_, OtrosPuntos), Puntos >= OtrosPuntos).
+
+
+totalPuntos(Agente, Puntos):- agente(Agente), 
+    findall(UnPunto, puntosPorTarea(Agente, UnPunto), ListaPuntos), sumlist(ListaPuntos, Puntos).
+
+puntosPorTarea(Agente, Puntos):-tarea(Agente, Tarea, _), puntos(Tarea, Puntos).
+
+puntos(vigilar(Negocios), Puntos):- length(Negocios, Cantidad), Puntos is Cantidad * 5.
+puntos(ingerir(_, Tamanio, Cantidad), Puntos):- unidadesIngeridas(Tamanio, Cantidad, Unidades), Puntos is Unidades * (-10).
+puntos(apresar(_, Recompensa), Puntos):- Puntos is Recompensa / 2.
+puntos(asuntosInternos(AgenteInvestigado), Puntos):- totalPuntos(AgenteInvestigado, PuntosAgente), Puntos is PuntosAgente * 2.
+
+unidadesIngeridas(Tamanio, Cantidad, Unidades):- Unidades is Tamanio * Cantidad.
+
+/*
+agentePremiado(Agente).
+Agente = sargentoGarcia
+*/
